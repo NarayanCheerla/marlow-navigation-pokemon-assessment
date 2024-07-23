fe7627e770f5beb2c3d168e251479c07c41f6d64
CNR <narayana.cheerla@gmail.com>
2024-07-23 21:11:47 +0530

    config scripts updated

--- packages/components/lib/Card.tsx
+++ packages/components/lib/Card.tsx
@@ -6,8 +6,11 @@ export interface CardProps {
 }
 const Card = ({children, wrapperClassNames}: CardProps) => {
     return (
-        <div className={wrapperClassNames}>
+        <div className={`${wrapperClassNames} flex text-red-600`}>
+            <>
+            <span className="text-lime-600">Test</span>
             {children}
+            </>
         </div>
     )
 }
--- packages/pokedex/src/app/page.tsx
+++ packages/pokedex/src/app/page.tsx
@@ -5,7 +5,9 @@ import { Card, Button} from "components";
 const HomePage = () => {
     return (
         <div className="flex items-center justify-center">
-            <Card wrapperClassNames="border rounded-lg p-10 m-2 bg-slate-500">Welcome to Custom Card component 2</Card>
+            <Card wrapperClassNames="border rounded-lg p-10 m-2 bg-slate-500">
+                Welcome to Custom Card component 4  
+                </Card>
             <h1 className="text-green-500">Welome to Home page {utils()}</h1>
         </div>
     )
